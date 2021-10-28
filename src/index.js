704924329f47212ef169d5d0cacd3d0dd45181f3
mrkrokrodile <gaetan.beurel@gmail.com>
2021-10-28 13:18:35 +0200

    CV

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 const App = () => {
     return (
         <div>
-             <h1>Homepage</h1>
-             <h2>HOLA QUETAL MUY BIEN Y TOO</h2>
+            <div id="homepage">
+                <div id="side">
+
+                      <h1 id="grand-titre">Gaëtan Beurel</h1>
+                      <h2>Développeur web</h2>
+
+               </div>
+                <div id="main-block">
+                    <div className="nes-container is-rounded">
+                        <div className="containers">
+                             <div class="nes-container with-title is-centered">
+                                 <p class="title">Connaissances</p>
+                                 <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+                             </div> 
+                        </div>
+                    </div>
+                </div>
+              
+            </div>
+           
+            
+             
         </div>
        
         
